83d90e7d1f9b8c79f07ca6021ee412d755b04508
Elana Kopelevich <ekopelevich@gmail.com>
2016-10-07 09:37:32 -0600

    progress on planit detail view

--- public/javascripts/controllers/ProjectsController.js
+++ public/javascripts/controllers/ProjectsController.js
@@ -1,13 +1,11 @@
 function ProjectsController(projectsService){
   var vm = this
   vm.projectList = getProjects()
-
   function getProjects() {
     return projectsService.readData()
       .then(projects => {
         vm.projectList = projects.data
+        return vm.projectsList
       })
   }
-
-
 }
