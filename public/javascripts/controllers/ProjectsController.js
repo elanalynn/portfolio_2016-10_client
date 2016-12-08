d78bfb643c1cffd76f635026982f77bb55ff8426
Elana Kopelevich <ekopelevich@gmail.com>
2016-12-08 14:50:12 -0700

    style collabowrite page

--- public/javascripts/controllers/ProjectsController.js
+++ public/javascripts/controllers/ProjectsController.js
@@ -1,8 +1,32 @@
-function ProjectsController(projectsService){
+function ProjectsController(projectsService, $state){
   var vm = this
+
   projectsService.readData()
   .then(projects => {
     vm.projectList = projects.data
+
+    switch ($state.current.name) {
+      case 'collabowrite': vm.projectIndex = 0
+        break
+      case 'stumblr': vm.projectIndex = 1
+        break
+      case 'planit': vm.projectIndex = 2
+        break
+      case 'reasonabout': vm.projectIndex = 4
+        break
+      case 'weeks': vm.projectIndex = 3
+        break
+      default: vm.projectIndex = 0
+
+    }
+
+    vm.getTech = projectIndex => {
+      vm.technologies = vm.projectList[projectIndex].technologies.split(', ')
+    }
+
+    vm.getTech(vm.projectIndex)
+
     return vm.projectsList
   })
+
 }
